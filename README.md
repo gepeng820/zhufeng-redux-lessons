453c204a03a8703c1d6fb5c4b18f9cbda9940286
zhufengnodejs <zhufengnodejs@126.com>
2017-05-25 00:21:15 +0800

    2. redux+react第一版

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Redux
 - 1. redux+jquery
+- 2. redux+react第一版
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
-import $ from 'jquery';
-$(document.body).append(`
- <p id="counter"></p>
- <button id="increase">+</button>
-`);
 const initState = {number: 0};
 const counter = (state = initState, action) => {
     if (action.type === 'ADD')
-        return {number:state.number+1};
+        return {number: state.number + 1};
     else
         return state;
 }
 const store = createStore(counter);
-$('#increase').on('click', function () {
-    store.dispatch({type: 'ADD'});
-});
-store.subscribe(() => {
-    $('#counter').html(store.getState().number);
-})
+class Counter extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{store.getState().number}</p>
+                <button onClick={()=>store.dispatch({type:'ADD'})}>+</button>
+            </div>
+        )
+    }
+}
+let render = () => {
+    ReactDOM.render(<Counter/>, document.querySelector('#root'));
+}
+store.subscribe(render);
+render();
